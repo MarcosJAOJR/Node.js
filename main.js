6dd7602be07c9a4226eb72afb926688ba27088e4
Marcos Augusto <mjaojr@gmail.com>
2015-07-15 00:22:33 -0300

    troca da porta do servidor

--- main.js
+++ main.js
@@ -10,5 +10,5 @@ http.createServer(function (req, res) {
 
   res.end(JSON.stringify({ version: packageJson.version }));
 
-}).listen(1337);
-console.log('servidor iniciado http://localhost:1337');
+}).listen(8080);
+console.log('servidor iniciado http://localhost:8080');
